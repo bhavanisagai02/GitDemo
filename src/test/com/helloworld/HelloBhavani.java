ed8e565e5d1e6a3dcb1fdfebc6f1cd4676973d33
Bhavani.Basavaraj <bhavanisagai02@gmail.com>
2025-04-10 11:24:56 +0530

    first commit

---
+++ src/test/com/helloworld/HelloBhavani.java
@@ -0,0 +1,17 @@
+package test.com.helloworld;
+
+public class HelloBhavani {
+	public static void main(String[] args) {
+	
+	    // Attributes
+	    String brand = null;
+	    int speed = 10;
+
+	    // Method
+	   // void drive() {
+	        System.out.println(brand + " is driving at " + speed + " km/h");
+	    //}
+	
+	}
+
+}
